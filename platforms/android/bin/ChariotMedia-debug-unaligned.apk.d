37c0670f9c1f970dab0d043d97ebe3b8bacd696b
Ken Rimple <krimple@chariotsolutions.com>
2013-08-29 10:25:03 -0400

    updates to install angular-ui

---
+++ platforms/android/bin/ChariotMedia-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/ChariotMedia-debug-unaligned.apk : \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/ChariotMedia.ap_ \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes.dex \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/libs/cordova-3.0.0.jar \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes.dex : \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/BuildConfig.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/ChariotMedia.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/R$attr.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/R$drawable.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/R$layout.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/R$string.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/R$xml.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/classes/io/cordova/hellocordova/R.class \
+/Users/kenrimple/git-repositories/chariot-media/platforms/android/bin/dexedLibs/cordova-3.0.0-a51988d23243199b6b72dee6654c65be.jar \
